6c739cd62b5de5d6e5e647f0e04059de0a29cfe5
AJOY SAHA <ajoy.code.dreamer@gmail.com>
2025-03-30 23:19:54 +0600

    some issues find out

--- js/script.js
+++ js/script.js
@@ -1,52 +1,50 @@
 // ============ typeJs ==========
 // ============== multiple line typeJs ==============
 
-// let typing = document.querySelectorAll(".typing");
-// let typing_array = Array.from(typing);
+let typing = document.querySelectorAll(".typing");
+let typing_array = Array.from(typing);
 
-// typing_array.map((line) => {
-//   let singleLineText = Array.from(line.innerHTML);
 
-//   singleLineText.map((text) => {
-//     line.innerHTML = "";
-//     let typeStart = 0;
+typing_array.map((line) => {
+  let singleLineText = Array.from(line.innerHTML);
+  line.innerHTML = ""
+  let typeStart = 0
 
-//     function typeJs() {
-//       line.innerHTML += text[typeStart];
-//       typeStart++;
-//       if (typeStart > singleLineText.length) {
-//         typeStart = 0;
-//         line.innerHTML =""
-//       }
-//     }
-//     let typeInterval = setInterval(() => {
-//       typeJs();
-//     }, 200);
+  function typeJs() {
+    typing.innerHTML += line[typeStart];
+    typeStart++;
+    if (typeStart > typing.length) {
+      line.innerHTML =""
+      typeStart = 0;
+    }
+  }
+  let typeInterval = setInterval(() => {
+    typeJs();
+  }, 200);
 
-//   });
 
-// });
+});
 
 // ============== multiple line typeJs ==============
 
 // =========== single line typeJs ============
 
-let typing = document.querySelector(".typing");
-let type_array = Array.from(typing.innerHTML)
-let typeStart = 0
-typing.innerHTML = ""
+// let typing = document.querySelector(".typing");
+// let type_array = Array.from(typing.innerHTML)
+// let typeStart = 0
+// typing.innerHTML = ""
 
-function typeJs(){
-  typing.innerHTML += type_array[typeStart]
-  typeStart++
-  if( typeStart > type_array.length){
-    typing.innerHTML =""
-    typeStart = 0
-  }
-}
-let typeInterval = setInterval(()=>{
-  typeJs()
-},200)
+// function typeJs(){
+//   typing.innerHTML += type_array[typeStart]
+//   typeStart++
+//   if( typeStart > type_array.length){
+//     typing.innerHTML =""
+//     typeStart = 0
+//   }
+// }
+// let typeInterval = setInterval(()=>{
+//   typeJs()
+// },200)
 
 // =========== single line typeJs ============
 
